9d499e6944ded49ac15dcad7fe6b604b7bcd0210
daniaokuye <rs_lidn@163.com>
2018-07-20 10:36:58 +0800

    check L1 on gamma/scalar

--- units.py
+++ units.py
@@ -1,4 +1,4 @@
-from layers.params import global_param, global_dropout
+from layers.params import global_param, global_dropout, sw
 from layers.dy_conv import assign_mask
 from mxnet import nd
 import mxnet, os
@@ -35,24 +35,70 @@ def init_sphere(mnet, loaded_model, ctx=mxnet.cpu()):
             v.initialize(mxnet.initializer.Xavier(magnitude=3), ctx=ctx)
     # load exist paramers
     mnet.load_params(loaded_model, ctx=ctx, allow_missing=True)
-    gammas = {k: v.data() for k, v in mnet.collect_params().items() if 'gamma' in k}
+    gammas = [k for k in mnet.collect_params().keys() if 'gamma' in k]
     return gammas
 
 
-def load_gamma(gammas, dropout=False, ratio=0.2):
+def static(data):
+    mu = nd.sum(nd.abs(data))
+    std = nd.sum(data * data)
+    count = nd.sum(data != 0)
+    # all_count = reduce(lambda x, y: x * y, masked.shape)
+    mu = mu / count
+    std = std - count * nd.power(mu, 2)
+    std = nd.sqrt(std / count)
+    return mu, std
+
+
+def load_gamma_test(mnet, dropout=False, ratio=0.2):
     # load gamma in the net to update mask
+    loss_g = mxnet.gluon.loss.L1Loss()
     loss = []
-    for key, gamma in gammas.items():
+    for key, value in mnet.collect_params().items():
+        if not 'gamma' in key:
+            continue
+        gamma = value.data()
+        # global_param.netMask[key] = assign_mask(gamma, global_param.netMask[key], key)
+        # mask = 1 - global_param.netMask[key]
+        # # if dropout, some fliters (ratio) will freeze
+        # if dropout:
+        #     # Dmask = global_dropout.get_select(mask, key, ratio)
+        #     k = int(ratio * gamma.shape[0])
+        #     Dmask = nd.topk(gamma, k=k, ret_typ='mask', is_ascend=True)
+        #     mask = (mask + Dmask) % 2
+        tag_key = '_'.join(key.split('_')[1:])
+        sw.add_scalar(tag=tag_key, value=nd.mean(gamma).asscalar(), global_step=global_param.iter)
+        target = nd.zeros_like(gamma).as_in_context(gamma.context)
+        this_loss = loss_g(gamma, target)
+        loss.append(nd.sum(this_loss / gamma.shape[0]))
+
+    out = reduce(lambda x, y: x + y, loss)
+    return out / len(loss)
+
+
+def load_gamma(mnet, dropout=False, ratio=0.2):
+    # load gamma in the net to update mask
+    loss_g = mxnet.gluon.loss.L1Loss()
+    loss = []
+    for key, value in mnet.collect_params().items():
+        if not 'gamma' in key:
+            continue
+        gamma = value.data()
         global_param.netMask[key] = assign_mask(gamma, global_param.netMask[key], key)
         mask = 1 - global_param.netMask[key]
         # if dropout, some fliters (ratio) will freeze
         if dropout:
-            Dmask = global_dropout.get_select(mask, key, ratio)
+            # Dmask = global_dropout.get_select(mask, key, ratio)
+            k = int(ratio * gamma.shape[0])
+            Dmask = nd.topk(gamma, k=k, ret_typ='mask', is_ascend=True)
             mask = (mask + Dmask) % 2
+        mine = mask * gamma
+        target = nd.zeros_like(mine).as_in_context(mine.context)
+        this_loss = loss_g(mine, target)
+        loss.append(nd.sum(this_loss / gamma.shape[0]))
 
-        loss.append(loss_gamma(mask, gamma))
     out = reduce(lambda x, y: x + y, loss)
-    return out
+    return out / len(loss)
 
 
 def loss_gamma(mask, weight):
