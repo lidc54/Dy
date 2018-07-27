ce639ff2e6f0fc4dd35bc42ce3d7d1df9b096485
daniaokuye <rs_lidn@163.com>
2018-07-27 15:30:45 +0800

    those paramers needed adjust frequectly set into class

--- layers/params.py
+++ layers/params.py
@@ -11,11 +11,23 @@ nums_power = 1
 advanced_iter = 0.5  # defalut is 1:not to advanced
 zoom = 5.0
 
-root = 'log_4dy_Ns4'
-sw = SummaryWriter(logdir=root, flush_secs=5)
+
+class global_sw(object):
+    def __init__(self):
+        super(global_sw, self).__init__()
+        self.sw = None
+
+    def set_sw(self, file):
+        self.sw = SummaryWriter(logdir=file, flush_secs=5)
+gls=global_sw()
+
 kept_in_kernel = 3
 
 alpha = 1  # harder punishment for loss
+HXW = 112 * 96
+
+
+# compressed_root = 'log_4dy_Ns3/transfed'
 
 
 class mask_param(object):
@@ -55,6 +67,45 @@ def get_params():
 
     print('o')
 
+
+prefix = ['net.0.a0.alpha', 'net.3.a0.alpha', 'net.1.conv0.weight', 'net.7.conv1.weight',
+          'net.1.conv1.bias', 'net.4.conv1.bias', 'net.0.conv0.bias', 'net.7.conv1.bias',
+          'net.7.a1.alpha', 'net.4.a2.alpha', 'net.2.conv2.weight', 'net.1.conv0.bias',
+          'net.1.conv1.weight', 'net.3.a1.alpha', 'net.4.conv2.weight', 'net.3.conv0.bias',
+          'net.3.a2.alpha', 'net.0.a1.alpha', 'net.3.conv2.weight', 'net.0.conv0.weight',
+          'net.8.weight', 'net.6.a1.alpha', 'net.3.conv0.weight', 'net.6.conv2.weight',
+          'f6.weight', 'net.6.conv1.weight', 'net.5.conv1.weight', 'net.1.a2.alpha',
+          'net.2.conv1.weight', 'net.2.conv1.bias', 'net.0.conv2.bias', 'net.7.a0.alpha',
+          'net.6.a2.alpha', 'net.1.a0.alpha', 'net.2.conv2.bias', 'net.4.conv1.weight',
+          'net.6.conv2.bias', 'net.5.a1.alpha', 'net.2.a1.alpha', 'net.2.a2.alpha',
+          'net.1.conv2.bias', 'net.3.conv1.weight', 'net.0.a2.alpha', 'net.0.conv2.weight',
+          'net.6.conv1.bias', 'net.1.a1.alpha', 'net.5.conv2.weight', 'net.5.conv2.bias',
+          'net.3.conv2.bias', 'net.1.conv2.weight', 'net.5.a2.alpha', 'net.0.conv1.bias',
+          'net.5.conv1.bias', 'net.7.conv0.weight', 'net.7.conv0.bias', 'net.0.conv1.weight',
+          'net.4.conv2.bias', 'net.7.conv2.bias', 'net.4.a1.alpha', 'net.7.conv2.weight',
+          'net.3.conv1.bias', 'net.8.bias', 'net.7.a2.alpha']
+
+collect = ['spherenet200_conv0_weight', 'spherenet200_conv0_bias', 'spherenet200_mprelu0_alpha',
+           'spherenet200_conv1_weight', 'spherenet200_conv1_bias', 'spherenet200_mprelu1_alpha',
+           'spherenet200_conv2_weight', 'spherenet200_conv2_bias', 'spherenet200_mprelu2_alpha',
+           'spherenet200_conv3_weight', 'spherenet200_conv3_bias', 'spherenet200_mprelu3_alpha',
+           'spherenet200_conv4_weight', 'spherenet200_conv4_bias', 'spherenet200_mprelu4_alpha',
+           'spherenet200_conv5_weight', 'spherenet200_conv5_bias', 'spherenet200_mprelu5_alpha',
+           'spherenet200_conv6_weight', 'spherenet200_conv6_bias', 'spherenet200_mprelu6_alpha',
+           'spherenet200_conv7_weight', 'spherenet200_conv7_bias', 'spherenet200_mprelu7_alpha',
+           'spherenet200_conv8_weight', 'spherenet200_conv8_bias', 'spherenet200_mprelu8_alpha',
+           'spherenet200_conv9_weight', 'spherenet200_conv9_bias', 'spherenet200_mprelu9_alpha',
+           'spherenet200_conv10_weight', 'spherenet200_conv10_bias', 'spherenet200_mprelu10_alpha',
+           'spherenet200_conv11_weight', 'spherenet200_conv11_bias', 'spherenet200_mprelu11_alpha',
+           'spherenet200_conv12_weight', 'spherenet200_conv12_bias', 'spherenet200_mprelu12_alpha',
+           'spherenet200_conv13_weight', 'spherenet200_conv13_bias', 'spherenet200_mprelu13_alpha',
+           'spherenet200_conv14_weight', 'spherenet200_conv14_bias', 'spherenet200_mprelu14_alpha',
+           'spherenet200_conv15_weight', 'spherenet200_conv15_bias', 'spherenet200_mprelu15_alpha',
+           'spherenet200_conv16_weight', 'spherenet200_conv16_bias', 'spherenet200_mprelu16_alpha',
+           'spherenet200_conv17_weight', 'spherenet200_conv17_bias', 'spherenet200_mprelu17_alpha',
+           'spherenet200_conv18_weight', 'spherenet200_conv18_bias', 'spherenet200_mprelu18_alpha',
+           'spherenet200_conv19_weight', 'spherenet200_conv19_bias', 'spherenet200_mprelu19_alpha',
+           'spherenet200_dense0_weight', 'spherenet200_dense0_bias', 'spherenet200_anglelinear0_weight']
 
 if __name__ == "__main__":
     get_params()
